5b7aa70111572f647209dda0eb348dd362bcb57d
Nathan Smith <nathanbsmith729@gmail.com>
2026-04-26 17:06:54 +0000

    Add prompting file

---
+++ Experiments/multi_model_ipo/prompt_orchestration/prompt_models.py
@@ -0,0 +1,71 @@
+from openai import OpenAI
+import os
+import pandas as pd
+from datetime import datetime
+from ..prompts.deep_research_prompt import create_deep_research_prompt
+from ..prompts.daily_prompt import create_daily_prompt
+from ..prompts.starting_prompt import create_starting_prompt
+
+def prompt_deepseek(text: str, model: str = "deepseek-chat") -> str:
+
+    deepseek_client = OpenAI(
+    api_key=os.environ["DEEPSEEK_API_KEY"],
+    base_url="https://api.deepseek.com",)
+    
+    response = deepseek_client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": text}],
+        temperature=0.0,
+    )
+
+    if not response.choices:
+        raise RuntimeError("No choices returned from DeepSeek.")
+
+    content = response.choices[0].message.content
+    if content is None:
+        raise RuntimeError("Output from DeepSeek was None.")
+
+    return content
+
+
+def prompt_chatgpt(text: str, model: str = "gpt-4.1-mini") -> str:
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": text}],
+        temperature=0.0,
+    )
+
+    if not response.choices:
+        raise RuntimeError("No choices returned from ChatGPT.")
+
+    content = response.choices[0].message.content
+    if content is None:
+        raise RuntimeError("Output from ChatGPT was None.")
+
+    return content
+
+def prompt_deep_research(libb) -> tuple[str, str]:
+    model = libb._model_path.replace("Experiments/multi_model_ipo/artifacts/", "")
+    # start date
+    if libb.run_date == pd.Timestamp("2026-04-24"):
+        text = create_starting_prompt(libb)
+    else:
+        text = create_deep_research_prompt(libb)
+        
+    if model == "deepseek":
+        return prompt_deepseek(text), text
+    elif model == "gpt-4.1":
+        return prompt_chatgpt(text), text
+    else:
+        raise RuntimeError(f"Unidentified model: {model}")
+
+def prompt_daily_report(libb) -> tuple[str, str]:
+    model = libb._model_path.replace("Experiments/multi_model_ipo/artifacts/", "")
+    text = create_daily_prompt(libb)
+    if model == "deepseek":
+        return prompt_deepseek(text), text
+    elif model == "gpt-4.1":
+        return prompt_chatgpt(text), text
+    else:
+        raise RuntimeError(f"Unidentified model: {model}")
